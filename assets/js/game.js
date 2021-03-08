422e0c201fb48045fde7d0c1099ddca2a3071f4e
Your Name <you@example.com>
2021-03-08 15:13:05 -0500

    game 01

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,40 @@
-window.alert("This is an alert! JavaScript is running");
+var fight= function() {
+    window.alert("Welcome to Robot Gladiators!");
+
+    enemyHealth= enemyHealth-playerAttack;
+    console.log(playerName + " attacked " + enemyName +"." + enemyName + " has now " + enemyHealth + " health remaining ")
+
+    if (enemyHealth <=0){
+        window.alert(enemyName+ " had died! ");
+    }
+    else {
+        window.alert(enemyName + " still has "+ enemyHealth + " health left.");
+    }
+
+    playerHealth= playerHealth-enemyAttack;
+    console.log(enemyName + " attacked " + playerName + "." + playerName + " has now " + playerHealth + " health remaining")
+
+    if (playerHealth <=0){
+        window.alert(playerName+ " had died! ");
+    }
+    else {
+        window.alert(playerName + " still has "+ playerHealth + " health left.");
+}
+};
+
+
+var playerName= window.prompt("What is your robot's name");
+var playerHealth= 100;
+var playerAttack= 10;
+
+console.log(playerName, playerHealth, playerAttack);
+
+var enemyName= "Roborto";
+var enemyHealth= 50;
+var enemyAttack= 12;
+
+console.log(enemyName, enemyHealth, enemyAttack);
+
+
+
+fight()
