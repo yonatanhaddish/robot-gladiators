ec49c7cae995d6338c1435782a6787196c29281f
Your Name <you@example.com>
2021-03-09 17:57:23 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,12 +14,27 @@ var fight= function(enemyName) {
     while(enemyHealth>0 && playerHealth>0) {
         var promptFight= window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose." );
 
-    if (promptFight === "fight" || promptFight === "FIGHT") {
+     if (promptFight==="SKIP" || promptFight==="skip") {
+        var confirmSkip= window.confirm("Are you sure you would like to quit?");
+        
+        if (confirmSkip){
+             window.alert( playerName + " has decided to skip this fight with "+ enemyName);
+            playerMoney= playerMoney-10;
+            console.log("playerMoney", playerMoney);
+            break;
+        }
+        else {
+            fight();
+            }   
+    }    
+
+    else if (promptFight === "fight" || promptFight === "FIGHT") {
         enemyHealth= enemyHealth-playerAttack;
         console.log(playerName + " attacked " + enemyName +"." + enemyName + " has now " + enemyHealth + " health remaining ")
 
         if (enemyHealth <=0){
             window.alert(enemyName+ " had died! ");
+            playerMoney= playerMoney + 20;
             break;
         }
         else {
@@ -30,26 +45,12 @@ var fight= function(enemyName) {
         console.log(enemyName + " attacked " + playerName + "." + playerName + " has now " + playerHealth + " health remaining")
     
         if (playerHealth <=0){
-            window.alert(playerName+ " had died! ");
+            window.alert(playerName+ " has died!");
             break;
         }
         else {
             window.alert(playerName + " still has "+ playerHealth + " health left.");
         }
-    }
-
-    else if (promptFight==="SKIP" || promptFight==="skip") {
-    var confirmSkip= window.confirm("Are you sure you would like to quit?");
-
-    if (confirmSkip){
-            window.alert( playerName + " has decided to skip this fight. Goodbye!");
-            playerMoney= playerMoney-10;
-            console.log("playerMoney", playerMoney);
-            break;
-        }
-    else {
-        fight();
-        }   
     }
 
     else window.alert("You need to choose a valid option. Try again !")
@@ -60,8 +61,16 @@ var fight= function(enemyName) {
 
 
 for (var i= 0; i<enemyNames.length; i++) {
-    console.log("You are fighting with " + enemyNames[i]);
+    if (playerHealth >0) {
+        window.alert("Welcome to Robot Gladiators! Round "+ (i+1));
+        console.log("You are now fighting with " + enemyNames[i]);
     var pickedEnemyName= enemyNames[i];
     enemyHealth= 50;
     fight(pickedEnemyName);
+    }
+    
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
